eed14ffaa8c6d86ff0ff4ced7f77dcc475ff02b4
Tachyon Speedster <prabhasjoshi100@gmail.com>
2025-12-14 19:39:55 +0530

    Delete SingularityOS directory

--- SingularityOS/src/idt.h
+++
@@ -1,8 +0,0 @@
-// src/idt.h
-
-// Function to load the IDT register (defined in boot.asm)
-extern void load_idt(void*); 
-
-// Functions for I/O port manipulation
-extern unsigned char inb(unsigned short port);
-extern void outb(unsigned short port, unsigned char data);
